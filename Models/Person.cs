39ee61891f13640e657b1c8f086f852e42a79175
jsakamoto <junichi.jp@gmail.com>
2016-03-22 21:39:17 +0900

    Initial commit.

---
+++ Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingJSONTextFileAsADataStore.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{{Id:{0}, Name:{1}, Age:{2}}}", this.Id, this.Name, this.Age);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Using JSON text file as a data store
+
+ | JSON text file | RDBMS (like MySQL)
+-|----------------|--------------------
+Need Addtional Installation | :) Nothing | :( Need
+Memory usage for small data | :) Enough low | :( use for RDBMS runtime
+Memory usage for large data | :( Because on memory process | :) Can treat large data
+Transactional/Atomic operation | :( No support | :) Rich support
