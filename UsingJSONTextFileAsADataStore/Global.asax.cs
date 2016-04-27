e893e5b3a22a35d5c780ebf4b240de531c3bcbdb
jsakamoto <junichi.jp@gmail.com>
2016-04-27 22:17:18 +0900

    Provide data store service object as a global object.

--- UsingJSONTextFileAsADataStore/Global.asax.cs
+++ UsingJSONTextFileAsADataStore/Global.asax.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Web.Routing;
 using System.Web.Mvc;
+using UsingJSONTextFileAsADataStore.Services;
 
 namespace UsingJSONTextFileAsADataStore
 {
     public class Global : System.Web.HttpApplication
     {
+        public static PeopleStore PeopleStore { get; private set; }
+
         protected void Application_Start(object sender, EventArgs e)
         {
+            Global.PeopleStore = new PeopleStore();
+
             RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             RouteTable.Routes.MapRoute(
