ad52b1f1184bedaedde7e45d771d1dd8fbad20e4
jsakamoto <junichi.jp@gmail.com>
2016-04-27 21:59:30 +0900

    Start building web edition.

---
+++ UsingJSONTextFileAsADataStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsingJSONTextFileAsADataStore.Controllers
+{
+    public class HomeController : System.Web.Mvc.Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UsingJSONTextFileAsADataStore/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace UsingJSONTextFileAsADataStore
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+    }
+}
